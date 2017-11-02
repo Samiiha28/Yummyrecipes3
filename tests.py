c02e7f3d67ed109cc8a4087ffbb40dfb4b133584
Samiiha28 <nsamiiha@gmail.com>
2017-11-02 20:02:38 +0300

    updated tests

--- tests.py
+++ tests.py
@@ -1,77 +1,33 @@
 import unittest
-from models import User,Category,Recipe
+from models import User,Recipe
 
 class UserTest(unittest.TestCase):
     def setUp(self):
         self.user = User("samiiha", "nsamiiha", "password")
 
    
-    def test_add_category(self):
-        self.assertEqual( self.user.add_category("dinner"),
-                        "recipe category is added succesfully")
+    def test_add_recipe(self):
+        self.assertEqual( self.user.add_recipe("matooke"),True)
 
-    def test_add_category_name_already_exists(self):
-        self.user.add_category("dinner")
-        self.assertEqual( self.user.add_category
-                         ("dinner"),
-                         "recipe category already exists")
+    def test_add_recipename_already_exists(self):
+        self.user.add_recipe("matooke")
+        self.assertEqual( self.user.add_recipe("matooke"),False)
 
 
-    def test_edit_category_not_found(self):
-        self.assertEqual( self.user.edit_category("absent", "newtype"),
-                         "not title found")
+    def test_edit_recipe(self):
+        self.assertEqual( self.user.edit_recipe("absent", "newtype"),False)
 
     def test_edit_category_successful(self):
-        self.user.add_category("Snacks")
-        self.assertEqual( self.user.edit_category("Snacks","local foods"),"recipe_category edited succesfully")
+        self.user.add_recipe("Snacks")
+        self.assertEqual( self.user.edit_recipe("Snacks","local foods"),True)
         
     def test_delete_category_not_found(self):
-        self.assertEqual( self.user.delete_category("not exist"), "category not found")
+        self.assertEqual( self.user.delete_recipe("not exist"), False)
 
     def test_delete_category_deleted(self):
-        self.user.add_category("lunch recipes")
-        self.assertEqual( self.user.delete_category("lunch recipes"),
-                         "category deleted")
-        
-    
-class Recipe_categoryTest(unittest.TestCase):
-    
-    def setUp(self):
-        self.recipes = Category("")
-
-    def test_recipe_instantiation(self):
-        self.assertIsInstance(self.recipes, Category,
-                              "Failed to instantiate")
-
-    def test_add_recipe_added(self):
-        self.assertEqual(self.recipes.add_recipe("pillawo",'discription'), "recipe added succesfully")
-
-    def test_add_recipe_exists(self):
-        self.recipes.add_recipe("pillawo",'description')
-        self.assertEqual(self.recipes.add_recipe(
-            "pillawo",'description'), "recipe already exists")
-
-    def test_edit_recipe_not_found(self):
-        self.assertEqual(self.recipes.edit_recipe(
-            "chicken recipe", "beef recipe"), "description not found")
-
-   
-    def test_edit_recipe_edited_succesfully(self):
-         self.recipes.add_recipe("pizza",'description')
-         self.assertEqual(self.recipes.edit_recipe("pizza", "chicken"), "description not found")
-
-    def test_delete_recipe_not_found(self):
-        self.assertEqual(self.recipes.delete_recipe(
-        "katogo",'description'), "No recipe to delete")
-
-    def test_recipe_deleted(self):
-        self.recipes.add_recipe("katogo",'description')
-        self.assertEqual(self.recipes.delete_recipe("katogo",'description'), "recipe deleted")
+        self.user.add_recipe("lunch recipes")
+        self.assertEqual( self.user.delete_recipe("lunch recipes"),True)
 
 class RecipeTest(unittest.TestCase):
     def setUp(self):
-        self.recipe = Recipe("title","description")
-
-
-    def test_create_item_instance(self):
-        self.assertIsInstance(self.recipe, Recipe, "Failed to create instance")
+        self.recipe = Recipe("description")
